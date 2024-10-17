448d4d10004c0d3a3855e8d4942d9685c2b09013
Keran02 <94802040+Keran02@users.noreply.github.com>
2024-10-17 15:50:39 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -63,7 +63,7 @@ cityInput.addEventListener("keyup", function (event) {
 
     var cityInputValue = cityInput.value;
 
-    var apiKey = "b1fd6e14799699504191b6bdbcadfc35"; // Default
+    var apiKey = "7d3582ada4896dccd3e42654d40246c4"; // Default
     var unit = "metric";
     var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInputValue}&appid=${apiKey}&units=${unit}`;
 
